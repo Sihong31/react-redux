42bdbe54ff9eb70cb7a00243460d6f03506aee33
Sihong Huang <sihongspring2011@gmail.com>
2018-12-20 13:26:35 -0500

    s13-react-redux-songs

---
+++ s13-react-redux-songs/src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+    return [
+        { title: 'No Mana', duration: '4:05'},
+        { title: 'Macarena', duration: '2:30'},
+        { title: 'All Star', duration: '3:15'},
+        { title: 'I want it that way', duration: '1:45'}
+    ];
+}
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SELECT_SONG') {
+        return action.payload;
+    }
+
+    return selectedSong;
+}
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});

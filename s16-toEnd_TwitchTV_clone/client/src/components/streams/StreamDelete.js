fb127fc1978b21dc80edc4dc3c357103e773a40a
Sihong Huang <sihongspring2011@gmail.com>
2018-12-25 18:28:53 -0500

    work on deleting a stream, deletion modal and setting up actions

--- s16-toEnd_TwitchTV_clone/client/src/components/streams/StreamDelete.js
+++ s16-toEnd_TwitchTV_clone/client/src/components/streams/StreamDelete.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-const StreamDelete = () => {
-    return (
-        <div>StreamDelete</div>
-    )
-};
+import { fetchStream, deleteStream } from '../../actions';
+import history from '../../history';
+import Modal from '../Modal';
 
-export default StreamDelete;
+class StreamDelete extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id); 
+    }
+
+    renderActions() {
+        const streamId = this.props.match.params.id;
+        return (
+            <React.Fragment>
+                <button onClick={ () => this.props.deleteStream(streamId) } className="ui button negative">Delete</button>
+                <Link to="/" className="ui button">Cancel</Link>
+            </React.Fragment>
+        );
+    }
+
+    renderContent() {
+        if (!this.props.stream) {
+            return 'Are you sure you want to delete this stream?'
+        }
+
+        return `Are you sure you want to delete this stream with title: ${this.props.stream.title}`
+    }
+
+    render() {
+        return (
+            <Modal
+                title="Delete Stream"
+                content={ this.renderContent() }
+                actions= { this.renderActions() }
+                onDismiss = { () => history.push('/') } 
+            />
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id]}
+}
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);

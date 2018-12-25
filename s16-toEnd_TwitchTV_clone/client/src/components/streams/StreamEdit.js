42dba48251f520ca2eb45b0b2557969e578eca87
Sihong Huang <sihongspring2011@gmail.com>
2018-12-25 13:24:17 -0500

    work on rest stream app

--- s16-toEnd_TwitchTV_clone/client/src/components/streams/StreamEdit.js
+++ s16-toEnd_TwitchTV_clone/client/src/components/streams/StreamEdit.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const StreamEdit = () => {
-    return (
-        <div>StreamEdit</div>
-    )
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
+
+class StreamEdit extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    onSubmit = (formValues) => {
+        console.log(formValues);
+    }
+
+    render() {
+        if (!this.props.stream) {
+            return <div> Loading... </div>
+        }
+        return (
+            <div>
+                <h3>Edit a stream</h3>
+                <StreamForm 
+                    initialValues = { this.props.stream }
+                    onSubmit={ this.onSubmit } 
+                />
+            </div>
+        )
+    }
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    return {stream: state.streams[ownProps.match.params.id]};
+}
+
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
